aa222aaaad05be4e2ba592796f2b87f59e3e003f
nonvi <70644671+nonvi@users.noreply.github.com>
2020-09-02 17:23:03 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -3,10 +3,15 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = input('inserire indirizzo ip:')
 port = int(input('inserire la porta:'))
+nickname = input('seleziona nickname:')
+
+
 
 serversocket.bind((host,port))
 
-serversocket.listen(100)
+
+serversocket.listen(2)
+
 
 clientsocket, addr = serversocket.accept()
 
@@ -16,7 +21,8 @@ while True:
   if i == 2:
     a = input('inserisci il messaggio:')
     b = a.encode()
-    clientsocket.send(b)
+    c = nickname.encode()
+    clientsocket.send(c + b':' + b)
     i = i-1
 
   else:
