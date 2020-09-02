acb855874ab903dae9cbfea75d44479b3e7e679a
nonvi <70644671+nonvi@users.noreply.github.com>
2020-09-02 17:21:58 +0200

    Update client.py

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = input('inserire indirizzo ip:')
 port = int(input('inserire la porta:'))
+nickname = input('seleziona nickname:')
 
 s.connect((host,port))
 
@@ -14,7 +15,8 @@ while True:
   if i == 2:
     a = input('inserisci il messagio:')
     b = a.encode()
-    s.send(b)
+    c = nickname.encode()
+    s.send(c + b':' + b)
     i = i-1
 
   else:
